70c98438497cbeca61a3e0f4bf95395ca1f87dbb
Dru Edmondson <peryton6@gmail.com>
2018-07-16 19:23:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -45,7 +45,8 @@ function removeFromCart(item) {
   //for loop checking if item === i, if true return .splice()
   for (let i=0; i < cart.length; i++){
     if (cart[i].itemName === item){
-      cart.splice(i, 1)
+      cart.splice(i, 1);
+      return cart
     }
     else
     return "That item is not in your cart."
