1c6a74fb72e07366da2bf4490965a7494424255e
Dru Edmondson <peryton6@gmail.com>
2018-07-16 19:19:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -46,12 +46,11 @@ function removeFromCart(item) {
   for (let i=0; i < cart.length; i++){
     if (cart[i].itemName === item){
       cart.splice(i, 1)
-
     }
     else{
     return "That item is not in your cart."
 }
-
+}
 function placeOrder(cardNumber) {
   // write your code here
 }
