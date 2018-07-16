eb2bbd2ec7e28eec5d9a396303d33e117601f32e
Dru Edmondson <peryton6@gmail.com>
2018-07-16 02:49:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,13 +10,29 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+ var price = Math.floor(Math.random() * (100) + 1);
+ var newItem = { itemName: item, itemPrice: price };
+ cart.push(newItem);
+return `${item} has been added to your cart.`;
 }
 
 function viewCart() {
-  // write your code here
+  var cartList = [];
+  if (cart.length === 0){
+    return "Your shopping cart is empty.";
+  }
+  else if (cart.length === 1){
+    return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`;
+  }
+  else{  
+    for (let i = 0; i < cart.length; i++){
+      cartList += `${cart[i].itemName} at $${cart[i].itemPrice}, `
+    }
+    cartList += `and ${cart[cart.length-1].itemName} at $${cart[cart.length-1].itemPrice}.`
+    return `In your cart, you have ${cartList}`
+  }
 }
-
+}
 function total() {
   // write your code here
 }
